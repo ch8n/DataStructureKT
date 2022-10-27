d54397587f4bfa0a068bfc32dbd81f1da086cae1
ch8n <chetan.garg36@gmail.com>
2022-10-27 19:03:38 +0530

    updated remote dependencies

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 import com.github.ch8n.linkedlist.LinkedList
+import com.github.ch8n.recursion.recursion
 
 fun main(args: Array<String>) {
     val linked = LinkedList()
     linked.test()
+
+    recursion { head { repeat(3) { println(it) } } }
 }
